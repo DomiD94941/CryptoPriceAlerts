2a04e3b09b51f782cbdc480e5c9da661dcb52918
Dominik Darłak <dominik.darlak@outlook.com>
2025-06-01 23:10:01 +0200

    Created Kafka Producer and Consumer

---
+++ consumer/analytics_consumer.py
@@ -0,0 +1,33 @@
+from confluent_kafka import Consumer
+import json
+
+conf = {
+    'bootstrap.servers': 'localhost:9093',
+    'group.id': 'analytics-group',
+    'auto.offset.reset': 'latest'
+}
+
+consumer = Consumer(conf)
+consumer.subscribe(['crypto_prices'])
+
+print("📥 Listening for crypto prices...")
+
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("Error:", msg.error())
+            continue
+
+        data = json.loads(msg.value().decode('utf-8'))
+        symbol = data.get('symbol')
+        price = data.get('price')
+        ts = data.get('timestamp')
+        print(f"✔ {symbol} @ {price} (ts: {ts})")
+
+except KeyboardInterrupt:
+    print("Stopping consumer...")
+finally:
+    consumer.close()
---
+++ producer/binance_producer.py
@@ -0,0 +1,33 @@
+import json
+import websocket
+from confluent_kafka import Producer
+
+producer = Producer({'bootstrap.servers': 'localhost:9093'})  
+TOPIC = 'crypto_prices'
+BINANCE_WS = "wss://stream.binance.com:9443/ws/btcusdt@trade"
+
+def on_message(ws, message):
+    data = json.loads(message)
+    payload = {
+        "symbol": data['s'],
+        "price": float(data['p']),
+        "timestamp": data['T']
+    }
+    producer.produce(TOPIC, json.dumps(payload))
+    producer.flush()
+    print("→ Sent:", payload)
+
+def on_error(ws, error): print("WebSocket Error:", error)
+def on_close(ws, close_status_code, close_msg): print("WebSocket Closed")
+def on_open(ws): print("Connected to Binance WebSocket")
+
+if __name__ == "__main__":
+    print("Starting Binance Kafka Producer..")
+    ws = websocket.WebSocketApp(
+        BINANCE_WS,
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+    )
+    ws.run_forever()
