33777c96b47431ccdd89bc77c8739f8251cdb352
Dominik Darłak <dominik.darlak@outlook.com>
2025-06-20 09:50:33 +0200

    Update init-ksql.sh

--- ksql/init/init-ksql.sh
+++ ksql/init/init-ksql.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 echo "[init-ksql.sh] Waiting for ksqlDB to be ready..."
-while ! curl -s http://ksqldb-server:8088/info > /dev/null; do
+until curl -s http://ksqldb-server:8088/info | grep -q "KsqlServerInfo"; do
   echo "  ... still waiting for ksqlDB..."
   sleep 2
 done
 
-echo "[init-ksql.sh] Executing all .sql files in order..."
-for f in /ksql-init/*.sql; do
-  echo "Executing $f ..."
-  ksql http://ksqldb-server:8088 < "$f"
+echo "[init-ksql.sh] Executing all SQL files in /ksql-init (sorted)..."
+
+for file in $(ls /ksql-init/*.sql | sort); do
+  echo ">>> Executing $file"
+  ksql http://ksqldb-server:8088 < "$file"
+  sleep 5
 done
+
+echo "[init-ksql.sh] All SQL files executed!"
